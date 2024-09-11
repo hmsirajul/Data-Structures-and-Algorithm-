4df358ab461555c440e2310e6653d45a057810ec
MD.SIRAJUL ISLAM <155076937+hmsirajul@users.noreply.github.com>
2024-09-12 00:18:00 +0600

    Heap Sort using function

---
+++ Heap/Heap Sort.cpp
@@ -0,0 +1,59 @@
+
+  #include<bits/stdc++.h>
+  using namespace std;
+
+  void heapify(int arr[], int n, int i) {
+
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < n && arr[left] > arr[largest])
+    {
+        largest = left;
+    }
+
+
+    if (right < n && arr[right] > arr[largest])
+    {
+
+        latest = right;
+    }
+
+
+    if (largest != i) {
+      swap(arr[i], arr[largest]);
+      heapify(arr, n, largest);
+    }
+  }
+
+
+  void buildHeap(int arr[], int n)
+  {
+
+    for (int i = n / 2 - 1; i >= 0; i--)
+    {
+        heapify(arr, n, i);
+
+    }
+
+
+
+    for (int i = n - 1; i >= 0; i--) {
+      swap(arr[0], arr[i]);
+      heapify(arr, i, 0);
+    }
+  }
+
+
+
+  int main() {
+    int arr[] = {1, 12, 9, 5, 6, 10};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    buildHeap(arr, n);
+    for(int i=0; i<n;i++)
+        cout << arr[i] << " ";
+    cout << endl;
+
+
+  }
