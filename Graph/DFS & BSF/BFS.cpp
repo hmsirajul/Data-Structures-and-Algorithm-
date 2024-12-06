e429fd009e0adb747ac0733fc231e3917fc017b5
MD.SIRAJUL ISLAM <155076937+hmsirajul@users.noreply.github.com>
2024-12-06 22:28:06 +0600

    Add files via upload

--- Graph/DFS & BSF/BFS.cpp
+++ Graph/DFS & BSF/BFS.cpp
@@ -1,25 +1,37 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-void dfs(vector<vector<int>> & adjList, vector<bool>& visited , int currentNode)
+void bfs(vector<vector<int>> &adjList, vector<bool> &visited, int startNode)
 {
-    visited[currentNode] = true;
-    cout<< currentNode << " ";
+    queue<int> q; // Create a queue for BFS
 
-    for(int neighbor : adjList[currentNode])
+    visited[startNode] = true; // Mark the startNode as visited
+    q.push(startNode);         // Enqueue the startNode
+
+    // Perform BFS
+    while (!q.empty())
     {
-        if(!visited[neighbor])
+        int currentNode = q.front();
+        q.pop();                    // Dequeue a vertex from queue
+        cout << currentNode << " "; // Print the dequeued vertex
+
+        // Visit all adjacent vertices of the dequeued vertex
+        for (int neighbor : adjList[currentNode])
         {
-            dfs(adjList, visited , neighbor);
+            if (!visited[neighbor])
+            {
+                visited[neighbor] = true; // Mark the unvisited neighbor as visited
+                q.push(neighbor);         // Enqueue the visited neighbor
+            }
         }
     }
 }
 
-int main ()
+int main()
 {
     int vertices = 6;
     vector<vector<int>> adjList(vertices);
-    vector<bool> visited(vertices,false);
+    vector<bool> visited(vertices, false);
 
     adjList[0] = {1, 3};
     adjList[1] = {0, 2, 4};
@@ -28,10 +40,10 @@ int main ()
     adjList[4] = {1, 3, 5};
     adjList[5] = {2, 4};
 
-
-    cout << "Depth First Traversal: ";
-    dfs(adjList, visited, 0);
+    cout << "BFS ";
+    bfs(adjList, visited, 0);
     cout << endl;
+    fill(visited.begin(), visited.end(), false);
 
     return 0;
 }
